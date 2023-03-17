6b7b5c95f93854b93e97308e33ef99b102d4001b
l33ass <ilua-smirnof@yandex.ru>
2023-03-17 16:27:07 +0100

    wip: begin importing js to svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,84 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  (async () => {
+    let volumeCallback = null;
+    let volumeInterval = null;
+    const volumeVisualizer = document.getElementById("volume-visualizer");
+    const startButton = document.getElementById("start");
+    const stopButton = document.getElementById("stop");
+    // Initialize
+    try {
+      const audioStream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+          echoCancellation: true,
+        },
+      });
+      const audioContext = new AudioContext();
+      const audioSource = audioContext.createMediaStreamSource(audioStream);
+      const analyser = audioContext.createAnalyser();
+      analyser.fftSize = 512;
+      analyser.minDecibels = -127;
+      analyser.maxDecibels = 0;
+      analyser.smoothingTimeConstant = 0.4;
+      audioSource.connect(analyser);
+      const volumes = new Uint8Array(analyser.frequencyBinCount);
+      volumeCallback = () => {
+        analyser.getByteFrequencyData(volumes);
+        let volumeSum = 0;
+        for (const volume of volumes) volumeSum += volume;
+        const averageVolume = volumeSum / volumes.length;
+        // Value range: 127 = analyser.maxDecibels - analyser.minDecibels;
+        volumeVisualizer?.style.setProperty(
+          "--volume",
+          (averageVolume * 100) / 127 + "%"
+        );
+      };
+    } catch (e) {
+      console.error(
+        "Failed to initialize volume visualizer, simulating instead...",
+        e
+      );
+    }
+    // Use
+    startButton?.addEventListener("click", () => {
+      // Updating every 100ms (should be same as CSS transition speed)
+      if (volumeCallback !== null && volumeInterval === null)
+        volumeInterval = setInterval(volumeCallback, 100);
+    });
+    stopButton?.addEventListener("click", () => {
+      if (volumeInterval !== null) {
+        clearInterval(volumeInterval);
+        volumeInterval = null;
+      }
+    });
+  })();
+</script>
+
+<div id="volume-visualizer" />
+<button id="start">Start</button>
+<button id="stop">Stop</button>
+
+<style>
+  div {
+    --volume: 0%;
+    position: relative;
+    width: 200px;
+    height: 20px;
+    margin: 50px;
+    background-color: #ddd;
+  }
+
+  div::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: var(--volume);
+    background-color: green;
+    transition: all 30ms ease-in-out;
+  }
+
+  button {
+    margin-left: 50px;
+  }
+</style>
