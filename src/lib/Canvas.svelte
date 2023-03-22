115b50f6b9e4704509af6824f8e416c4576e9d1c
l33ass <ilua-smirnof@yandex.ru>
2023-03-22 17:56:27 +0100

    wip: canvas rendering

---
+++ src/lib/Canvas.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let canvas
+</script>
+
+<canvas bind:this={canvas} width="640" height="640"></canvas>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,72 +1,108 @@
 <script>
   // @ts-nocheck
 
+  import Canvas from "./../lib/Canvas.svelte";
+
   let volumePercentage = "0%";
   let volumeInterval = null;
+  let volumeColor = 0;
+  //canvas global variables
+  let canvas;
 
   async function init() {
-    const audioStream = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        echoCancellation: true,
-      },
-    });
-    const audioContext = new AudioContext();
-    const audioSource = audioContext.createMediaStreamSource(audioStream);
-    const analyser = audioContext.createAnalyser();
-    analyser.fftSize = 512;
-    analyser.minDecibels = -127;
-    analyser.maxDecibels = 0;
-    analyser.smoothingTimeConstant = 0.4;
-    audioSource.connect(analyser);
-    const volumes = new Uint8Array(analyser.frequencyBinCount);
-    return {volumes, analyser};
-  };
+      const audioStream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+          echoCancellation: true,
+        },
+      });
+      const audioContext = new AudioContext();
+      const audioSource = audioContext.createMediaStreamSource(audioStream);
+      const analyser = audioContext.createAnalyser();
+      analyser.fftSize = 512;
+      analyser.minDecibels = -127;
+      analyser.maxDecibels = 0;
+      analyser.smoothingTimeConstant = 0.4;
+      audioSource.connect(analyser);
+      const volumes = new Uint8Array(analyser.frequencyBinCount);
+      return {volumes, analyser};
 
-  function volumeCallback(audio) {
-    audio.analyser.getByteFrequencyData(audio.volumes);
-    let volumeSum = 0;
-    let screamCounter = 0;
-    for (const volume of audio.volumes) volumeSum += volume;
-    const averageVolume = volumeSum / audio.volumes.length;
-    // Value range: 127 = analyser.maxDecibels - analyser.minDecibels;
-    volumePercentage = (averageVolume * 100) / 127 * 0.73;
-    console.log(volumePercentage > 80);
-  }
+    };
+    
+    function volumeCallback(audio) {
+      audio.analyser.getByteFrequencyData(audio.volumes);
+      let volumeSum = 0;
+      let screamCounter = 0;
+      for (const volume of audio.volumes) volumeSum += volume;
+      const averageVolume = volumeSum / audio.volumes.length;
+      // Value range: 127 = analyser.maxDecibels - analyser.minDecibels;
+      volumePercentage = (averageVolume * 100) / 127 * 0.73;
+      volumeColor = (1 - volumePercentage / 100) * 96;
+      //canvas rendering
+      let bar_count = window.innerWidth / 2;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.fillStyle = "#ffffff";
 
-  async function startListening() {
-    if (volumeInterval === null) {
-      let audio = await init();
-      volumeInterval = setInterval(volumeCallback, 100, audio);
+      for (let i = 0; i < bar_count; i++) {
+        bar_pos = i * 4;
+        bar_width = 2;
+        bar_height = -(audio.volumes[i] / 2);
+          
+        context.fillRect(bar_pos, height, bar_width, bar_height);
+      }
     }
-  }
 
-  function stopListening() {
-    if (volumeInterval !== null) {
-      clearInterval(volumeInterval);
-      volumeInterval = null;
+    async function handleListening() {
+      if (volumeInterval !== null) {
+        clearInterval(volumeInterval);
+        volumeInterval = null;
+      } else if (volumeInterval === null) {
+        let audio = await init();
+        volumeInterval = setInterval(volumeCallback, 100, audio);
+      }
     }
-  }
 </script>
 
 <div class="volume__container">
-  <div class="volume__visualizer" style="width:{volumePercentage + "%"}; background-color:hsl({(1 - volumePercentage / 100) * 160}, 100%, 50%)" />
+  <div class="volume__visualizer" style="background-color:hsl({volumeColor}, 83%, 50%)" />
 </div>
-<button on:click={startListening} disabled="{volumeInterval}" class="volume__start">Start</button>
-<button on:click={stopListening} disabled="{!volumeInterval}" class="volume__stop">Stop</button>
+<div class="volume__buttons">
+  <label class="t">
+    <input class="t__checkbox" type="checkbox" on:click={handleListening} value="on">
+    <svg class="t__svg" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <circle class="t__svg-ring" cx="12" cy="12" r="6" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-dasharray="0 5 27.7 5" stroke-dashoffset="0.01" transform="rotate(-90,12,12)"/>
+      <line class="t__svg-line" x1="12" y1="6" x2="12" y2="15" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-dasharray="9 9" stroke-dashoffset="3"/>
+      <line class="t__svg-line" x1="12" y1="6" x2="12" y2="12" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-dasharray="6 6" stroke-dashoffset="6"/>
+    </svg>
+    <span class="t__sr">Power</span>
+  </label>
+</div>
+
+<Canvas bind:canvas = {canvas}></Canvas>
 
 <style>
   .volume__container {
-    width:300px;
-    height:20px;
-    background-color: #ddd;
-    position: relative;
+    display:flex;
+    justify-content: center;
+    padding:1rem 0;
   }
-  
+
+  .volume__buttons {
+    display: flex;
+    justify-content: center;
+    font: 1em/1.5 sans-serif;
+  }
+
+  .volume__buttons * {
+    border: 0;
+    margin: 0;
+    padding: 0;
+  }
+
   .volume__visualizer {
-    position:absolute;
-    top:0;
-    left:0;
-    z-index:1;
-    height: 20px;
+    width:calc(3rem + 2vw); 
+    height:calc(3rem + 2vw);
+    background-color: #ddd;
+    border-radius: 50%;
+    position: relative;
   }
 </style>
