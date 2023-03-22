d46ebff9d1e58da77ddb4bbd3ce1491306c1d466
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-03-22 23:14:37 +0100

    add: audio equalizer render with canvas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,35 @@
 <script>
   // @ts-nocheck
-
-  import Canvas from "./../lib/Canvas.svelte";
+  import { onMount } from "svelte";
 
   let volumePercentage = "0%";
   let volumeInterval = null;
-  let volumeColor = 0;
   //canvas global variables
   let canvas;
+  let context;
+  let circleRadius = 75;
+  let canvasWidth = 300;
+  let canvasHeight = 300;
 
+  function drawCircle(context) {
+    context.beginPath();
+    context.arc(0, 0, circleRadius + 1, 0, 2 * Math.PI);
+    context.fill();
+  }
+
+  onMount(() => {
+    context = canvas.getContext('2d');
+    context.translate(canvasWidth / 2, canvasHeight / 2);
+    context.strokeStyle = "#fff";
+    context.fillStyle = "#fff";
+    context.lineWidth = 2;
+    drawCircle(context);
+  });
+
+  /**
+   * initializes audio device used
+   * @return Object{Uint8Array volumes, AnalyserNode analyser}
+   */
   async function init() {
       const audioStream = await navigator.mediaDevices.getUserMedia({
         audio: {
@@ -25,32 +46,38 @@
       audioSource.connect(analyser);
       const volumes = new Uint8Array(analyser.frequencyBinCount);
       return {volumes, analyser};
-
     };
     
+    /**
+     * interval callback that gets live audio and renders canvas
+     * @param audio
+     */
     function volumeCallback(audio) {
       audio.analyser.getByteFrequencyData(audio.volumes);
       let volumeSum = 0;
       let screamCounter = 0;
-      for (const volume of audio.volumes) volumeSum += volume;
+      for (const volume of audio.volumes) {
+        volumeSum += volume;
+      }
       const averageVolume = volumeSum / audio.volumes.length;
       // Value range: 127 = analyser.maxDecibels - analyser.minDecibels;
       volumePercentage = (averageVolume * 100) / 127 * 0.73;
       volumeColor = (1 - volumePercentage / 100) * 96;
       //canvas rendering
-      let bar_count = window.innerWidth / 2;
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.fillStyle = "#ffffff";
-
-      for (let i = 0; i < bar_count; i++) {
-        bar_pos = i * 4;
-        bar_width = 2;
-        bar_height = -(audio.volumes[i] / 2);
-          
-        context.fillRect(bar_pos, height, bar_width, bar_height);
+      let bars = 90;
+      context.clearRect(-200, -200, 500, 500);
+      drawCircle(context);
+      for (let i = 0; i < 180; i+= (180/bars)) {
+        let barHeight = audio.volumes[i] / 2;
+        let barWidth = 2 * Math.PI * circleRadius / bars;
+        context.rotate((Math.PI * 2) / bars);
+        context.fillRect(circleRadius, barWidth / 2, barHeight, barWidth);
       }
     }
 
+    /**
+     * On/off button handler
+     */
     async function handleListening() {
       if (volumeInterval !== null) {
         clearInterval(volumeInterval);
@@ -63,9 +90,9 @@
 </script>
 
 <div class="volume__container">
-  <div class="volume__visualizer" style="background-color:hsl({volumeColor}, 83%, 50%)" />
+  <canvas class="volume__equalizer" bind:this={canvas} width="{canvasWidth}" height="{canvasHeight}"></canvas>
 </div>
-<div class="volume__buttons">
+<div class="volume__button">
   <label class="t">
     <input class="t__checkbox" type="checkbox" on:click={handleListening} value="on">
     <svg class="t__svg" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -77,7 +104,6 @@
   </label>
 </div>
 
-<Canvas bind:canvas = {canvas}></Canvas>
 
 <style>
   .volume__container {
@@ -86,23 +112,15 @@
     padding:1rem 0;
   }
 
-  .volume__buttons {
+  .volume__button {
     display: flex;
     justify-content: center;
     font: 1em/1.5 sans-serif;
   }
 
-  .volume__buttons * {
+  .volume__button * {
     border: 0;
     margin: 0;
     padding: 0;
-  }
-
-  .volume__visualizer {
-    width:calc(3rem + 2vw); 
-    height:calc(3rem + 2vw);
-    background-color: #ddd;
-    border-radius: 50%;
-    position: relative;
   }
 </style>
