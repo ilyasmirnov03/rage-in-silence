179dd6976520a5bbecffe7da60970c51591bfa6b
Ilya Smirnov <ilua-smirnof@yandex.ru>
2023-03-27 18:47:18 +0200

    add: difficulty choice for the game

---
+++ src/lib/DifficultyChoice.svelte
@@ -0,0 +1,21 @@
+<script>
+    // @ts-nocheck
+    export let percentageCap = 0;
+
+    function changeDifficulty (cap) {
+        percentageCap = cap;
+    }
+</script>
+
+<div class="difficulty__container">
+    <button on:click={() => changeDifficulty(70)}>Easy</button>
+    <button on:click={() => changeDifficulty(60)}>Medium</button>
+    <button on:click={() => changeDifficulty(50)}>Hard</button>
+    <button on:click={() => changeDifficulty(30)}>Insane</button>
+</div>
+
+<style>
+    .difficulty__container {
+        display:flex;
+    }
+</style>
